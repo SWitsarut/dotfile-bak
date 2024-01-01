6ab853663cb5c89138c026eec6474fd1e7c12708
SueaWS <121418561+SWitsarut@users.noreply.github.com>
2024-01-01 17:11:11 +0700

    Create tree-sitter.lua

---
+++ nvim/lua/tree-sitter.lua
@@ -0,0 +1,26 @@
+return {
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
+		configs.setup({
+			ensure_installed = {
+				"c",
+				"lua",
+				"vim",
+				"vimdoc",
+				"query",
+				"elixir",
+				"heex",
+				"javascript",
+				"html",
+				"typescript",
+				"cpp",
+				"css",
+			},
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
+}
