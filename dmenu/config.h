af880922bd7993b63e0e18329104f3455ded734c
SWitsarut <witsarut1133@outlook.com>
2024-01-09 14:31:43 +0700

    backup

--- dmenu/config.h
+++ dmenu/config.h
@@ -4,14 +4,22 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"FiraCode Nerd Font:size=12"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+
+static const char col_gray1[]  = "#222222";
+static const char col_gray2[]  = "#bbbbbb";
+static const char col_gray3[]  = "#eeeeee";
+static const char col_black[]  = "#000000";
+static const char col_white[]  = "#ffffff";
+static const char col_primary[]= "#27343a";
+
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	/*               fg         bg          */
+	[SchemeNorm] = { col_gray2, col_gray1   },
+	[SchemeSel]  = { col_gray3, col_primary },
+	[SchemeOut]  = { col_black, col_white   },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
--- dwm/config.h
+++ dwm/config.h
@@ -3,19 +3,21 @@
 /* appearance */
 static const unsigned int borderpx  = 5;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 0;        /* 0 means no bar */
+static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "FiraCode Nerd Font:size=12" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
+static const char col_gray1[]       = "#888888";
+static const char col_gray2[]       = "#222222";
+static const char col_black[]       = "#000000";
+static const char col_white[]       = "#ffffff";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#fc7f0a";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_gray1, col_black, col_gray2 },
+	[SchemeSel]  = { col_white, col_black,  col_white  },
 };
 
 /* tagging */
